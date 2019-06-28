b9868d0422d15fd4414a5cd443dc75c3879329dd
markoAnon <mstevanovic160@gmail.com>
2019-06-29 00:38:01 +0200

    upload all files

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyARZ2xuy3xAZtl6Y2C75sxHoep2NeVyLdI';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=[API_KEY]';
         if (!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyARZ2xuy3xAZtl6Y2C75sxHoep2NeVyLdI';
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=[API_KEY]';
         }
         axios.post(url, authData)
             .then(response => {
